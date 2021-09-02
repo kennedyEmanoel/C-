83ad8c9fa42df44154e86365417ee3398b7dd5d5
kennedyEmanoel <79938217+kennedyEmanoel@users.noreply.github.com>
2021-09-02 09:19:01 -0300

    Commit inicial

---
+++ meuPrimeiroProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace meuPrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
